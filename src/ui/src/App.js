f43582e16592e142e0fa684a519f04779c57e691
Max Nelson <maxnelso@gmail.com>
2020-06-06 10:58:43 -0700

    Cleanup

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,12 +7,11 @@ state = {
   };
 
   componentDidMount() {
-      // Call our fetch function below once the component mounts
     this.callBackendAPI()
       .then(res => this.setState({ data: res.hello}))
       .catch(err => console.log(err));
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+
   callBackendAPI = async () => {
     const response = await fetch('/games');
     const body = await response.json();
@@ -21,18 +19,14 @@ state = {
     if (response.status !== 200) {
       throw Error(body.message)
     }
+
     return body;
   };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        // Render the newly fetched data inside of this.state.data
-        <p className="App-intro">{this.state.data}</p>
+      <div>
+        <p>{this.state.data}</p>
       </div>
     );
   }
