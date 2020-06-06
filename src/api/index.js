89c7673349861221e07a21f21fbd3eb511a4f83d
zaknelson <zak.nelson@gmail.com>
2020-06-06 12:05:48 -0700

    add basics of /games

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,34 @@
 const express = require('express');
-const route = express();
+const bodyParser = require('body-parser');
+const GameService = require('./services/GameService');
+const app = express();
 const port = 3000;
 
-route.get('/games', (req, res) => {
-  res.send({ hello: 'Hello World!' });
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get('/games', async (req, res) => {
+  res.send(GameService.getGames());
 });
 
-route.listen(port, () =>
+app.get('/games/:gameId', async (req, res) => {
+  res.send(GameService.getGame(req.params.gameId));
+});
+
+app.post('/games', async (req, res) => {
+  res.send(GameService.createGame(req.body));
+});
+
+app.listen(port, () =>
   console.log(`String Cheese API listening at http://localhost:${port}`)
 );
 
-route.post(
+app.post(
   '/',
   // validators.userSignup,
   async (req, res, next) => {
--- src/api/services/GameService.js
+++ src/api/services/GameService.js
@@ -1,16 +1,24 @@
-export default class UserService() {
+const _ = require('lodash');
+const shortid = require('shortid');
+const Game = require('../models/Game');
 
-    async Signup(user) {
-      const userRecord = await UserModel.create(user);
-      const companyRecord = await CompanyModel.create(userRecord); // needs userRecord to have the database id 
-      const salaryRecord = await SalaryModel.create(userRecord, companyRecord); // depends on user and company to be created
-      
-      ...whatever
-      
-      await EmailService.startSignupSequence(userRecord)
-  
-      ...do more stuff
-  
-      return { user: userRecord, company: companyRecord };
-    }
+let games = [];
+
+class GameService {
+  createGame(gameOptions) {
+    const game = new Game(gameOptions);
+    game.id = shortid.generate();
+    games.push(game);
+    return game;
   }
+
+  getGame(id) {
+    return _.find(games, { id });
+  }
+
+  getGames() {
+    return games;
+  }
+}
+
+module.exports = new GameService();
