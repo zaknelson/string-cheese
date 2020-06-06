e923719bcc3cbdf6b4b13027ce2be5abee97f4f0
zaknelson <zak.nelson@gmail.com>
2020-06-06 11:06:36 -0700

    add dev dependencies

---
+++ src/api/services/GameService.js
@@ -0,0 +1,16 @@
+export default class UserService() {
+
+    async Signup(user) {
+      const userRecord = await UserModel.create(user);
+      const companyRecord = await CompanyModel.create(userRecord); // needs userRecord to have the database id 
+      const salaryRecord = await SalaryModel.create(userRecord, companyRecord); // depends on user and company to be created
+      
+      ...whatever
+      
+      await EmailService.startSignupSequence(userRecord)
+  
+      ...do more stuff
+  
+      return { user: userRecord, company: companyRecord };
+    }
+  }
