a37af1d19cbea66158e816a0787552b07a3595ad
zaknelson <zak.nelson@gmail.com>
2020-06-07 15:10:45 -0700

    add drag and dropping

--- src/ui/src/CardGrid.js
+++ src/ui/src/CardGrid.js
@@ -3,6 +3,7 @@ import ButtonBase from '@material-ui/core/ButtonBase';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import _ from 'lodash';
 
 import './CardGrid.css';
@@ -14,30 +15,56 @@ class CardGrid extends Component {
 
   render() {
     return (
-      <Grid container className="root" spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justify="center" spacing={2}>
-            {_.range(this.props.cards.length).map((i) => (
-              <Grid key={this.props.cards[i].id} item>
-                <ButtonBase
-                  disabled={this.props.disabled}
-                  onClick={this.onClick.bind(this, this.props.cards[i])}
-                >
-                  <Paper className="paper">
-                    <Typography
-                      variant="h6"
-                      component="h6"
-                      className="textArea"
-                    >
-                      {this.props.cards[i].name}
-                    </Typography>
-                  </Paper>
-                </ButtonBase>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable" direction="horizontal">
+          {(provided, snapshot) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              <Grid container className="root" spacing={2}>
+                <Grid item xs={12}>
+                  <Grid container justify="center" spacing={2}>
+                    {_.range(this.props.cards.length).map((i) => (
+                      <Draggable
+                        key={this.props.cards[i].id}
+                        draggableId={this.props.cards[i].id}
+                        index={i}
+                      >
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <Grid key={this.props.cards[i].id} item>
+                              <ButtonBase
+                                disabled={this.props.disabled}
+                                onClick={this.onClick.bind(
+                                  this,
+                                  this.props.cards[i]
+                                )}
+                              >
+                                <Paper className="paper">
+                                  <Typography
+                                    variant="h6"
+                                    component="h6"
+                                    className="textArea"
+                                  >
+                                    {this.props.cards[i].name}
+                                  </Typography>
+                                </Paper>
+                              </ButtonBase>
+                            </Grid>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </Grid>
+                </Grid>
               </Grid>
-            ))}
-          </Grid>
-        </Grid>
-      </Grid>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     );
   }
 }
