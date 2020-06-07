7c8e8e555a3754a43374593fd905deda1522495e
zaknelson <zak.nelson@gmail.com>
2020-06-06 22:52:58 -0700

    add scoreboard

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
-import MainPage from './MainPage'
-import PickNamePage from './PickNamePage'
-import PlayerPage from './PlayerPage'
+import MainPage from './MainPage';
+import PickNamePage from './PickNamePage';
+import PlayerPage from './PlayerPage';
+import Scoreboard from './Scoreboard';
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={MainPage} />
-      <Route exact path="/games/:gameid" component={PickNamePage} />
-      <Route exact path="/games/:gameid/players/:playerid" component={PlayerPage} />
-    </Switch>
-  </Router>
-)
+  <div className="app">
+    <Scoreboard></Scoreboard>
+    <div className="page">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/games/:gameid" component={PickNamePage} />
+          <Route
+            exact
+            path="/games/:gameid/players/:playerid"
+            component={PlayerPage}
+          />
+        </Switch>
+      </Router>
+    </div>
+  </div>
+);
 
 export default App;
---
+++ src/ui/src/Scoreboard.js
@@ -0,0 +1,61 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import GradeIcon from '@material-ui/icons/Grade';
+import Card from '@material-ui/core/Card';
+import SearchIcon from '@material-ui/icons/Search';
+
+import './Scoreboard.css';
+
+class Scoreboard extends Component {
+  state = {
+    players: null,
+  };
+
+  componentDidMount() {
+    this.getPlayers().then((players) => {
+      this.setState({ players });
+    });
+  }
+
+  getPlayers = async (gameId, playerId) => {
+    gameId = 123; // TODO fix
+    playerId = 'abc'; // TODO fix
+    const response = await fetch('/games/' + gameId + '/players');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+
+    return body;
+  };
+
+  render() {
+    if (!this.state.players) {
+      return null;
+    }
+
+    let listItems = _.map(this.state.players, (player) => (
+      <ListItem key={player.id}>
+        <ListItemIcon>
+          <GradeIcon />
+        </ListItemIcon>
+        <ListItemText primary={player.name} />
+        <ListItemText secondary={player.points} />
+      </ListItem>
+    ));
+
+    return (
+      <Card className="Scoreboard">
+        <List>{listItems}</List>
+      </Card>
+    );
+  }
+}
+
+export default Scoreboard;
